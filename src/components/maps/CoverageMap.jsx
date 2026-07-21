d150838bb65e0348226997b0f39db2c847f66458
RoyerSMG <royersteven2004@gmail.com>
2026-07-21 18:50:35 -0500

    Update Marca en mapa

--- src/components/maps/CoverageMap.jsx
+++ src/components/maps/CoverageMap.jsx
@@ -1,7 +1,20 @@
+import L from "leaflet";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
 import { locations } from "../../data/locations";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+});
 
 export default function CoverageMap({
     locations,
