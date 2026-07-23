842542f621e224d767d7382f6ce591c2cc77be28
RoyerSMG <royersteven2004@gmail.com>
2026-07-23 18:02:42 -0500

    Se agrega operacion CEE

--- src/data/operations.jsx
+++ src/data/operations.jsx
@@ -75,5 +75,17 @@ export const operations = [
             "Recepción",
             "Despacho"
         ]
+    },
+    {
+        id: 7,
+        title: "CEE",
+        image: ejemplo,
+        document: alarmaPrincipalPDF,
+        shortDescription: "Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure",
+        description: "Lorem ipsum dolor sit amet consectetur, adipiscing elit imperdiet eu augue ac, natoque netus fusce pretium. Luctus duis viverra pulvinar habitasse platea a urna aliquet, tortor nisl nibh massa sociosqu hac placerat class lacus, dapibus quis ultricies mollis augue tincidunt ad. Nulla primis dis tempus feugiat sodales litora pretium etiam, magnis ante tempor rhoncus habitant nibh cras nisl, scelerisque non posuere himenaeos lobortis porttitor montes.",
+        functions: [
+            "Recepción",
+            "Despacho"
+        ]
     }
 ];
