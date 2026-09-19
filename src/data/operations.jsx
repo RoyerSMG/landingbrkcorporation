9c66bf1d9bb8fe472bbb620f456bb2f5a269c74f
RoyerSMG <royersteven2004@gmail.com>
2026-09-19 10:00:12 -0500

    update epago

--- src/data/operations.jsx
+++ src/data/operations.jsx
@@ -85,7 +85,7 @@ export const operations = [
     {
         id: 7,
         title: "E pago",
-        image: brk_Epago,
+        image: brkEpago,
         document: alarmaPrincipalPDF,
         shortDescription: "Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure",
         description: "Lorem ipsum dolor sit amet consectetur, adipiscing elit imperdiet eu augue ac, natoque netus fusce pretium. Luctus duis viverra pulvinar habitasse platea a urna aliquet, tortor nisl nibh massa sociosqu hac placerat class lacus, dapibus quis ultricies mollis augue tincidunt ad. Nulla primis dis tempus feugiat sodales litora pretium etiam, magnis ante tempor rhoncus habitant nibh cras nisl, scelerisque non posuere himenaeos lobortis porttitor montes.",
